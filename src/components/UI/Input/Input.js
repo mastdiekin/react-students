b201d65d226e6ab465103a86611de3e5e3fbdf97
Dmitriy Surkov <mastdiekin@gmail.com>
2020-05-15 18:55:46 +0300

    check validation

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,18 +1,30 @@
 import React from "react";
 import "./Input.sass";
+import misc from "../../../assets/sass/misc.sass";
 
 const input = (props) => {
+  let inputClasses = [];
+  if (props.invalid && props.shouldValidate && props.touched) {
+    inputClasses.push(misc.error);
+  }
+
   const typeInput = (
     <input
+      className={props.class + inputClasses.join(" ")}
       type={props.format}
       placeholder={props.placeholder}
       value={props.value}
+      onChange={props.changed}
     />
   );
   const typeTextarea = (
-    <textarea name={props.name} placeholder={props.placeholder}>
-      {props.value}
-    </textarea>
+    <textarea
+      className={props.class + inputClasses.join(" ")}
+      name={props.name}
+      placeholder={props.placeholder}
+      onChange={props.changed}
+      value={props.value}
+    ></textarea>
   );
   switch (props.type) {
     case "input":
--- src/components/containers/AddNewStudent/AddNewStudent.js
+++ src/components/containers/AddNewStudent/AddNewStudent.js
@@ -4,12 +4,9 @@ import Input from "../../UI/Input/Input";
 import Button from "../../UI/Button/Button";
 import Aux from "../../hoc/Auxx/Auxx";
 import misc from "../../../assets/sass/misc.sass";
+import { checkValid } from "../../hoc/shared/utility";
 
 class AddNewStudent extends Component {
-  state = {
-    show: false,
-  };
-
   toggle = (e) => {
     e.preventDefault();
     this.setState({
@@ -25,25 +22,165 @@ class AddNewStudent extends Component {
     document.body.style.overflow = "auto";
   };
 
+  state = {
+    form: {
+      lName: {
+        type: "input",
+        format: "text",
+        placeholder: "Фамилия",
+        value: "",
+        class: "",
+        required: true,
+        validation: {
+          required: true,
+          minLength: 3,
+        },
+        valid: false,
+        touched: false,
+      },
+      name: {
+        type: "input",
+        format: "text",
+        placeholder: "Имя",
+        value: "",
+        class: "",
+        required: true,
+        validation: {
+          required: true,
+          minLength: 2,
+        },
+        valid: false,
+        touched: false,
+      },
+      dateB: {
+        type: "input",
+        format: "text",
+        placeholder: "Дата рождения",
+        value: "",
+        class: "",
+        required: true,
+        validation: {
+          required: true,
+          minLength: 1,
+        },
+        valid: false,
+        touched: false,
+      },
+      faculty: {
+        type: "input",
+        format: "text",
+        placeholder: "Факультет",
+        value: "",
+        class: "",
+        required: true,
+        validation: {
+          required: true,
+          minLength: 3,
+        },
+        valid: false,
+        touched: false,
+      },
+      date: {
+        type: "input",
+        format: "text",
+        placeholder: "Дата поступления",
+        value: "",
+        class: "",
+        required: true,
+        validation: {
+          required: true,
+          minLength: 1,
+        },
+        valid: false,
+        touched: false,
+      },
+      course: {
+        type: "input",
+        format: "text",
+        placeholder: "Курс",
+        value: "",
+        class: "",
+        required: true,
+        validation: {
+          required: true,
+          minLength: 1,
+        },
+        valid: false,
+        touched: false,
+      },
+      adress: {
+        type: "textarea",
+        format: "text",
+        placeholder: "Адрес",
+        value: "",
+        class: "",
+        required: true,
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+        valid: false,
+        touched: false,
+      },
+      phone: {
+        type: "input",
+        format: "text",
+        placeholder: "Телефон",
+        value: "",
+        class: "",
+        required: true,
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+        valid: false,
+        touched: false,
+      },
+    },
+    show: true,
+  };
+
+  onChangeInput = (event, id) => {
+    const updatedControls = {
+      ...this.state.form,
+      [id]: {
+        ...this.state.form[id],
+        value: event.target.value,
+        valid: checkValid(event.target.value, this.state.form[id].validation),
+        touched: true,
+      },
+    };
+    this.setState({ form: updatedControls });
+  };
+
   render() {
+    const formElementsArray = [];
+    for (let key in this.state.form) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.form[key],
+      });
+    }
+
     let isShowModal;
     if (this.state.show) {
       isShowModal = (
         <Modal show={() => this.toggle()} closed={() => this.close()}>
           <form>
-            <Input format="text" type="input" placeholder="Имя" />
-            <Input format="text" type="input" placeholder="Фамилия" />
-            <Input
-              format="text"
-              type="input"
-              placeholder="Дата рождения"
-              class="error"
-            />
-            <Input format="text" type="input" placeholder="Факультет" />
-            <Input format="text" type="input" placeholder="Дата поступления" />
-            <Input format="text" type="input" placeholder="Курс" />
-            <Input format="text" type="textarea" placeholder="Адрес" />
-            <Input format="tel" type="input" placeholder="Телефон" />
+            {formElementsArray.map((el) => (
+              <Input
+                key={el.id}
+                format={el.config.format}
+                type={el.config.type}
+                placeholder={el.config.placeholder}
+                class={el.config.class}
+                value={el.config.value}
+                invalid={!el.config.valid}
+                shouldValidate={el.config.validation}
+                touched={el.config.touched}
+                changed={(event) => this.onChangeInput(event, el.id)}
+              />
+            ))}
             <Button
               btnClass={["Newstudent__submit", misc.btn, misc.big].join(" ")}
             >
