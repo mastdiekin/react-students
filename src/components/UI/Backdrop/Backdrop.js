0d4c4a74d3b4923ae1449bdf85b8721fafc4729c
Dmitriy Surkov <mastdiekin@gmail.com>
2020-05-16 21:09:45 +0300

    ref

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,24 +2,18 @@ import React from "react";
 import classes from "./Backdrop.sass";
 
 const backdrop = (props) => {
-  if (props.show) {
-    if (props.returnBack) {
-      return (
-        <div
-          className={classes.Backdrop}
-          onClick={() =>
-            props.returnBack.goBack(
+  return props.show ? (
+    <div
+      className={classes.Backdrop}
+      onClick={() =>
+        props.returnBack
+          ? props.returnBack.goBack(
               props.returnBack.location.state.background.pathname
             )
-          }
-        ></div>
-      );
-    } else {
-      return <div className={classes.Backdrop} onClick={props.closed}></div>;
-    }
-  } else {
-    return null;
-  }
+          : props.closed()
+      }
+    ></div>
+  ) : null;
 };
 
 export default backdrop;
