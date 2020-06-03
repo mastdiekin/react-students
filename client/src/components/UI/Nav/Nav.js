2e4823d4fdb55576a9abb7048accbd584fa3b207
Dmitriy Surkov <mastdiekin@gmail.com>
2020-06-03 23:22:20 +0300

    fix

--- client/src/components/UI/Nav/Nav.js
+++ client/src/components/UI/Nav/Nav.js
@@ -1,22 +1,50 @@
 import React from "react";
 import classes from "./Nav.sass";
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../../../store/actions";
 
-const nav = () => (
-  <nav className={classes.Nav}>
-    <ul>
-      <li>
-        <NavLink activeClassName={classes.active} exact to="/">
-          Главная
-        </NavLink>
-      </li>
-      <li>
-        <NavLink activeClassName={classes.active} to="/students">
-          Студенты
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
-);
+const Nav = (props) => {
+  const history = useHistory();
+  const clicked = (e) => {
+    e.preventDefault();
+    history.push("/students");
+    props.onInitStudents(1);
+  };
 
-export default nav;
+  return (
+    <nav className={classes.Nav}>
+      <ul>
+        <li>
+          <NavLink activeClassName={classes.active} exact to="/">
+            Главная
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            activeClassName={classes.active}
+            onClick={(e) => clicked(e)}
+            to="/students"
+          >
+            Студенты
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    students: state.students.students,
+    loading: state.students.loading,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onInitStudents: (page) => dispatch(actions.initStudents(page)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Nav);
